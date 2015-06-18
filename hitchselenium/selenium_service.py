3c5976601788b49df0e6529f6e0b2915a9c16df1
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-06-18 15:56:37 +0800

    FEATURE : Shunt the window to the top left

--- hitchselenium/selenium_service.py
+++ hitchselenium/selenium_service.py
@@ -6,10 +6,11 @@ import os
 
 class SeleniumService(Service):
 
-    def __init__(self, xvfb=False, **kwargs):
+    def __init__(self, xvfb=False, shunt_window=True, **kwargs):
         kwargs['log_line_ready_checker'] = lambda line: "READY" in line
         xvfb_run = ['xvfb-run'] if xvfb else []
         kwargs['command'] = xvfb_run + [sys.executable, "-u", "-m", "hitchselenium.server"]
+        self.shunt_window = shunt_window
         self._driver = None
         super(SeleniumService, self).__init__(**kwargs)
 
@@ -22,6 +23,12 @@ class SeleniumService(Service):
             self._driver.implicitly_wait(5.0)
             self._driver.accept_next_alert = True
             return self._driver
+
+    def poststart(self):
+        """Move the firefox window out of the way of the mouse."""
+        if self.shunt_window:
+            driver = self.driver
+            driver.set_window_position(0, 0)
 
     @Service.env_vars.getter
     def env_vars(self):
