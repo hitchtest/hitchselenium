65d0a0e6af2ddbb569bbc7812c10081c499c025d
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-10-20 16:40:46 +0800

    RELEASE : Bumped version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*parts):
     return codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts), 'r').read()
 
 setup(name="hitchselenium",
-      version="0.4.7",
+      version="0.4.8",
       description="Plugin to run Selenium with firefox using the Hitch testing framework.",
       long_description=read('README.rst'),
       classifiers=[
