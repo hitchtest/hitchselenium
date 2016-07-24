31c7af487c76fe7c04ebd28e36f8838834d83cf2
Colm O'Connor <colm.oconnor.github@gmail.com>
2016-07-24 11:57:09 +0100

    FEATURE : Turn on persistent logging of all requests made in the browser to make it easier to debug test runs.

--- hitchselenium/profile.py
+++ hitchselenium/profile.py
@@ -6,3 +6,4 @@ class Profile(webdriver.FirefoxProfile):
     def __init__(self, *args, **kwargs):
         super(Profile, self).__init__(*args, **kwargs)
         self.set_preference("startup.homepage_welcome_url.additional", "")
+        self.set_preference("devtools.webconsole.persistlog", True)
