d72342100c47b9154898962522e894535a67f1a5
Colm O'Connor <colm.oconnor.github@gmail.com>
2016-09-28 19:51:33 +0100

    BUG : Prevent automated upgrades, loading welcome screen URL and checking to see if ff is the default browser.

--- hitchselenium/profile.py
+++ hitchselenium/profile.py
@@ -5,5 +5,8 @@ import os
 class Profile(webdriver.FirefoxProfile):
     def __init__(self, *args, **kwargs):
         super(Profile, self).__init__(*args, **kwargs)
-        self.set_preference("startup.homepage_welcome_url.additional", "")
+        self.set_preference("browser.startup.homepage_override.mstone", "ignore")
+        self.set_preference("startup.homepage_welcome_url.additional", "about:blank")
+        self.set_preference("browser.shell.checkDefaultBrowser", False)
         self.set_preference("devtools.webconsole.persistlog", True)
+        self.set_preference("app.update.enabled", True)
